b6124f4ec0de56aeb539003fe48a9bbe8fd53a82
Christina Gerard <ChristinaGerard@Christinas-MBP.hsd1.ga.comcast.net>
2020-01-11 23:17:14 -0500

    able to add values to crystals in game.js file and console log them

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,36 @@
 $(function () {
     const log = console.log;
-    log('ready');
+    var crystal_values = [
+        {crystal: 'denim', value: 0},
+        {crystal: 'orange', value: 0},
+        {crystal: 'pink', value: 0},
+        {crystal: 'vintage', value: 0}
+    ]
 
-
-    $("img").on("click", () => {
-        log('image clicked');
+    $("img").on("click", (event) => {
+        var crystal = event.target.id;
     });
+
+
+    // Create a function that generates a random number
+    function randomNum(maxNum){
+        return Math.floor(Math.random() * maxNum) + 1;
+    }
+
+    // Create a function that gives the crystals a random 
+    // number value from 1-12
+    function giveCrystalsValue(){
+        for (var i = 0; i < crystal_values.length; i++){
+            crystal_values[i].value = randomNum(12);
+        }
+
+        log(crystal_values);
+    }
+
+    // Execute all functions that need to be ran at the begining of 
+    // the game here.
+    giveCrystalsValue();
+
 
 
 });
