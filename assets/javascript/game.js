cef02eeedf77cefbb5be4d53a7ba7892737ac94e
Christina Gerard <ChristinaGerard@Christinas-MBP.hsd1.ga.comcast.net>
2020-01-12 19:43:32 -0500

    updates user total each time a crystal is now clicked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,10 +5,29 @@ $(function () {
         {crystal: 'orange', value: 0},
         {crystal: 'pink', value: 0},
         {crystal: 'vintage', value: 0}
-    ]
+    ];
+
+    var user_total = 0;
+
+
+
 
     $("img").on("click", (event) => {
         var crystal = event.target.id;
+
+        log("Crystal clicked: ", crystal);
+        // add the value of the crystal clicked to user total
+        // then execute updateUserTotal()
+        
+        for (var a = 0; a < crystal_values.length; a++){
+            // if crystal clicked matches the crystal name in the
+            // array cystal_value and that value of the object to 
+            // the user total variable
+            if(crystal_values[a].crystal == crystal){
+                user_total += crystal_values[a].value;
+                updateUserTotal();
+            }
+        }
     });
 
 
@@ -31,10 +50,22 @@ $(function () {
         $("#winning_number").text(randomNum(102) + 18);
     }
 
+    // create a function that updates the user's total
+    function updateUserTotal(){
+        $("#user_total").text(user_total);
+    }
+
     // Execute all functions that need to be ran at the begining of 
     // the game here.
+
+    // Gives crystals the values for the game
     giveCrystalsValue();
+
+    // displays the winning number
     newWinningNum();
+
+    // displays initial user total
+    updateUserTotal();
 
     
 
